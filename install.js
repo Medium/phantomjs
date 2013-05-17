8328162d980b2c59a06d4d91e0da35e47d086b5b
Dan Pupius <dan@medium.com>
2013-05-17 13:11:27 -0400

    Use tmp from npm config; clean up code using promises

--- install.js
+++ install.js
@@ -9,6 +9,7 @@
 var cp = require('child_process')
 var fs = require('fs')
 var http = require('http')
+var kew = require('kew')
 var path = require('path')
 var url = require('url')
 var rimraf = require('rimraf').sync
@@ -18,9 +19,7 @@ var ncp = require('ncp')
 var npmconf = require('npmconf')
 
 var libPath = path.join(__dirname, 'lib', 'phantom')
-var tmpPath = process.env.TMPDIR ? path.join(process.env.TMPDIR, 'phantomjs') : path.join(__dirname, 'tmp')
 var downloadUrl = 'http://phantomjs.googlecode.com/files/phantomjs-' + helper.version + '-'
-var fileName
 
 if (process.platform === 'linux' && process.arch === 'x64') {
   downloadUrl += 'linux-x86_64.tar.bz2'
@@ -36,34 +35,62 @@ if (process.platform === 'linux' && process.arch === 'x64') {
 }
 
 var fileName = downloadUrl.split('/').pop()
-var downloadedFile = path.join(tmpPath, fileName)
-
-// Start the install.
-if (fs.existsSync(downloadedFile)) {
-  console.log('Download already available in tmpdir')
-  console.log(downloadedFile)
-  extractIt()
-} else {
-  fetchIt()
-}
 
 
-function mkdir(name) {
-  var dir = path.dirname(name)
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir)
+npmconf.load(function(err, conf) {
+  if (err) {
+    console.log('Error loading npm config')
+    console.error(err)
+    process.exit(1)
+    return
   }
-}
 
-function getOptions(proxyUrl) {
+  var tmpPath = path.join(conf.get('tmp'), 'phantomjs')
+  var downloadedFile = path.join(tmpPath, fileName)
+
+  var promise= kew.resolve(true)
+
+  // Start the install.
+  if (!fs.existsSync(downloadedFile)) {
+    promise = promise.then(function () {
+      rimraf(tmpPath)
+      mkdir(downloadedFile)
+      console.log('Downlaoading', downloadUrl)
+      console.log('Saving to', downloadedFile)
+      return requestBinary(getRequestOptions(conf.get('proxy')), downloadedFile)
+    })
+
+  } else {
+    console.log('Download already available at', downloadedFile)
+  }
+
+  promise.then(function () {
+    return extractDownload(downloadedFile, tmpPath)
+  })
+  .then(function () {
+    return copyIntoPlace(tmpPath, libPath)
+  })
+  .then(function () {
+    return fixFilePermissions()
+  })
+  .then(function () {
+    console.log('Done. Phantomjs binary available at', helper.path)
+  })
+  .fail(function (err) {
+    console.error('Phantom installation failed', err.stack)
+    process.exit(1)
+  })
+})
+
+
+function getRequestOptions(proxyUrl) {
   if (proxyUrl) {
     var options = url.parse(proxyUrl)
     options.path = downloadUrl
     options.headers = { Host: url.parse(downloadUrl).host }
     // turn basic authorization into proxy-authorization
     if (options.auth) {
-      options.headers['Proxy-Authorization'] = 'Basic ' +
-              new Buffer(options.auth).toString('base64')
+      options.headers['Proxy-Authorization'] = 'Basic ' + new Buffer(options.auth).toString('base64')
       delete options.auth
     }
 
@@ -73,131 +100,108 @@ function getOptions(proxyUrl) {
   }
 }
 
-function finishIt(err, stdout, stderr) {
-  // copy phantomjs folder to lib folder 
-  function copyIntoPlace (folder, unpackTarget, cb) {
-    var start = Date.now()
-    ncp(folder, unpackTarget, function (er) {
-      if (er) {
-        console.log('File copy failed.')
-        console.error(err);
-        process.exit(1)
-        return;
-      }
-      cb(er)
-    });
-  }
 
-  function afterRename(err) {
-    // For isolating extraction problems, https://github.com/Obvious/phantomjs/issues/15
-    if (err) {
-      console.log('Temporary files not renamed, maybe zip extraction failed.')
-      process.exit(1)
-      return
-    }
+function requestBinary(requestOptions, filePath) {
+  var deferred = kew.defer()
 
-    // Check that the binary is user-executable and fix it if it isn't (problems with unzip library)
-    if (process.platform != 'win32') {
-      var stat = fs.statSync(helper.path)
-      // 64 == 0100 (no octal literal in strict mode)
-      if (!(stat.mode & 64)) {
-        console.log('Fixing file permissions')
-        fs.chmodSync(helper.path, '755')
-      }
-    }
-
-    console.log('Done. Phantomjs binary available at', helper.path)
-  }
-
-  if (err) {
-    console.log('Error extracting archive', err)
-    process.exit(1)
-  } else {
-    // Look for the extracted directory, so we can rename it.
-    var files = fs.readdirSync(tmpPath)
-    for (var i = 0; i < files.length; i++) {
-      var file = path.join(tmpPath, files[i])
-      if (fs.statSync(file).isDirectory()) {
-        console.log('Renaming extracted folder', files[i], ' -> phantom')
-        copyIntoPlace(file, libPath, afterRename)
-      }
-    }
-  }
-}
-
-function extractIt() {
-  var options = {cwd: tmpPath}
-
-  rimraf(libPath)
-
-  if (fileName.substr(-4) === '.zip') {
-    console.log('Extracting zip contents')
-
-    try {
-      var zip = new AdmZip(downloadedFile)
-      zip.extractAllTo(path.dirname(downloadedFile), true)
-      finishIt()
-    } catch (e) {
-      finishIt(e)
-    }
-
-  } else {
-    console.log('Extracting tar contents (via spawned process)')
-    cp.execFile('tar', ['jxf', downloadedFile], options, finishIt)
-  }
-}
-
-function fetchIt() {
-  var notifiedCount = 0
   var count = 0
+  var notifiedCount = 0
+  var outFile = fs.openSync(filePath, 'w')
 
-  rimraf(tmpPath)
-
-  mkdir(downloadedFile)
-
-  var outFile = fs.openSync(downloadedFile, 'w')
-
-  function onData(data) {
-    fs.writeSync(outFile, data, 0, data.length, null)
-    count += data.length
-    if ((count - notifiedCount) > 800000) {
-      console.log('Received ' + Math.floor(count / 1024) + 'K...')
-      notifiedCount = count
-    }
-  }
-
-  function onEnd() {
-    console.log('Received ' + Math.floor(count / 1024) + 'K total.')
-    fs.closeSync(outFile)
-    extractIt()
-  }
-
-  function onResponse(response) {
+  var client = http.get(requestOptions, function (response) {
     var status = response.statusCode
     console.log('Receiving...')
 
     if (status === 200) {
-      response.addListener('data', onData)
-      response.addListener('end', onEnd)
+      response.addListener('data',   function (data) {
+        fs.writeSync(outFile, data, 0, data.length, null)
+        count += data.length
+        if ((count - notifiedCount) > 800000) {
+          console.log('Received ' + Math.floor(count / 1024) + 'K...')
+          notifiedCount = count
+        }
+      })
+
+      response.addListener('end',   function () {
+        console.log('Received ' + Math.floor(count / 1024) + 'K total.')
+        fs.closeSync(outFile)
+        deferred.resolve(true)
+      })
+
     } else {
-      console.log('Error with http request', response.headers)
       client.abort()
-      process.exit(1)
+      deferred.reject('Error with http request: ' + util.inspect(response.headers))
+    }
+  })
+
+  return deferred.promise
+}
+
+
+function extractDownload(filePath, tmpPath) {
+  var deferred = kew.defer()
+  var options = {cwd: tmpPath}
+
+  rimraf(libPath)
+
+  if (filePath.substr(-4) === '.zip') {
+    console.log('Extracting zip contents')
+
+    try {
+      var zip = new AdmZip(filePath)
+      zip.extractAllTo(path.dirname(filePath), true)
+      deferred.resolve(true)
+    } catch (e) {
+      deferred.reject('Error extracting archive ' + err.stack)
+    }
+
+  } else {
+    console.log('Extracting tar contents (via spawned process)')
+    cp.execFile('tar', ['jxf', filePath], options, function (err, stdout, stderr) {
+      if (err) {
+        deferred.reject('Error extracting archive ' + err.stack)
+      } else {
+        deferred.resolve(true)
+      }
+    })
+  }
+  return deferred.promise
+}
+
+
+function copyIntoPlace(tmpPath, targetPath) {
+  var deferred = kew.defer()
+  // Look for the extracted directory, so we can rename it.
+  var files = fs.readdirSync(tmpPath)
+  for (var i = 0; i < files.length; i++) {
+    var file = path.join(tmpPath, files[i])
+    if (fs.statSync(file).isDirectory()) {
+      console.log('Renaming extracted folder', file, '->', targetPath)
+      ncp(file, targetPath, deferred.makeNodeResolver())
+      break
     }
   }
+  return deferred.promise
+}
 
-  npmconf.load(function(err, conf) {
-    if (err) {
-        console.log('Error loading npm config')
-        console.error(err)
-        process.exit(1)
-        return
+
+
+function fixFilePermissions() {
+  // Check that the binary is user-executable and fix it if it isn't (problems with unzip library)
+  if (process.platform != 'win32') {
+    var stat = fs.statSync(helper.path)
+    // 64 == 0100 (no octal literal in strict mode)
+    if (!(stat.mode & 64)) {
+      console.log('Fixing file permissions')
+      fs.chmodSync(helper.path, '755')
     }
+  }
+}
 
-    var proxyUrl = conf.get('proxy')
-    var client = http.get(getOptions(proxyUrl), onResponse)
 
-    console.log('Downlaoading', downloadUrl)
-    console.log('Saving to', downloadedFile)
-  })
+function mkdir(name) {
+  var dir = path.dirname(name)
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir)
+  }
 }
