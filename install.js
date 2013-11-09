b1fcc1ef05bbdb7253415e6d348d7be19eab2b43
Nick Gordon <ngordon779@gmail.com>
2013-11-08 20:30:39 -0800

    support for freebsd as well

--- install.js
+++ install.js
@@ -83,7 +83,7 @@ whichDeferred.promise
       downloadUrl += 'linux-i686.tar.bz2'
     } else if (process.platform === 'darwin') {
       downloadUrl += 'macosx.zip'
-    } else if (process.platform === 'openbsd') {
+    } else if (process.platform === 'openbsd' || process.platform === 'freebsd') {
       downloadUrl += 'macosx.zip'
     } else if (process.platform === 'win32') {
       downloadUrl += 'windows.zip'
