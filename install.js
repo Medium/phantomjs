20013c42c41b95ef8088e1e783a205274877580e
Paul Hawxby <phawxby@gmail.com>
2017-04-25 18:20:49 +0100

    Switching to decompress as it suppot tar.gz

--- install.js
+++ install.js
@@ -8,7 +8,7 @@
 
 var requestProgress = require('request-progress')
 var progress = require('progress')
-var extractZip = require('extract-zip')
+var decompress = require('decompress')
 var cp = require('child_process')
 var fs = require('fs-extra')
 var helper = require('./lib/phantomjs')
@@ -270,35 +270,22 @@ function extractDownload(filePath) {
   // extract to a unique directory in case multiple processes are
   // installing and extracting at once
   var extractedPath = filePath + '-extract-' + Date.now()
-  var options = {cwd: extractedPath}
 
   fs.mkdirsSync(extractedPath, '0777')
   // Make double sure we have 0777 permissions; some operating systems
   // default umask does not allow write by default.
   fs.chmodSync(extractedPath, '0777')
 
-  if (filePath.substr(-4) === '.zip') {
-    console.log('Extracting zip contents')
-    extractZip(path.resolve(filePath), {dir: extractedPath}, function(err) {
-      if (err) {
-        console.error('Error extracting zip')
-        deferred.reject(err)
-      } else {
-        deferred.resolve(extractedPath)
-      }
-    })
+  console.log('Extracting archive')
+  decompress(path.resolve(filePath), extractedPath)
+    .then(function(files) {
+      deferred.resolve(extractedPath)
+    },
+    function(err) {
+      console.error('Error extracting zip')
+      deferred.reject(err)
+    });
 
-  } else {
-    console.log('Extracting tar contents (via spawned process)')
-    cp.execFile('tar', ['jxf', path.resolve(filePath)], options, function (err) {
-      if (err) {
-        console.error('Error extracting archive')
-        deferred.reject(err)
-      } else {
-        deferred.resolve(extractedPath)
-      }
-    })
-  }
   return deferred.promise
 }
 
